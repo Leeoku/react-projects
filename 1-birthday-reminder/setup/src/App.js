43579bd4847362a79ca806a36e2b56826b717163
Leeoku <leeoku@gmail.com>
2020-12-04 00:09:09 -0500

    finish birthday project

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const clearList = () => {
+    setPeople([]);
+  };
+  console.log(people)
+  return (
+    <section className="container">
+      <p>There are {people.length} birthday(s) today!</p>
+      <List people={people}></List>
+      <button onClick={clearList}>Clear All</button>
+    </section>
+  );
 }
 
 export default App;
